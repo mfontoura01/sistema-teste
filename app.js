b0513d859f17cb1065ded8729512294f47c1e517
Marcelo Carneiro da Fontoura <72932432+mfontoura01@users.noreply.github.com>
2024-09-19 14:46:31 -0300

    Update app.js inclui 3 linhas

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação está se conectando ao Banco de Dados")
+// mais um console log de teste: feito por Mfontoura01
+console.log("Nova alteração para ver o reflexo no VSCode do notebook")
